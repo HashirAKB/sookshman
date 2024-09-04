e2128a1081650ce3c73e1a10b6e917ad52554590
HashirAKB <ahmedhashir96@gmail.com>
2024-09-04 23:18:09 +0530

    Fix: NavBar Hydration Error.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="max-w-7xl mx-auto flex flex-col gap-4 p-4 min-h-[92vh]">
     <Navbar/>
+    <div>Hi</div>
     </main>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+// "use client"
 import { Box } from "lucide-react";
 import React from "react";
 import { ModeToggle } from "./ui/mode-toggle";
